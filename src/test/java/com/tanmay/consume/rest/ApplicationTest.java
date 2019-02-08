960b268df4031fc357bb6b333b36f35ed3811e2e
t@nm@y <writetotkpatra@gmail.com>
2019-02-09 03:21:02 +0530

    initial commit

--- src/test/java/com/tanmay/consume/rest/ApplicationTest.java
+++ src/test/java/com/tanmay/consume/rest/ApplicationTest.java
@@ -14,27 +14,19 @@
  * limitations under the License.
  */
 
-package hello;
-
-import static org.assertj.core.api.Assertions.assertThat;
+package com.tanmay.consume.rest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
 	
-	@Autowired
-	private RestTemplate restTemplate;
-
 	@Test
 	public void contextLoads() {
-		assertThat(restTemplate).isNotNull();
 	}
 
 }
